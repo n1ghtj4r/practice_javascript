0e835050d4f568a4e6983e17d28e74d55c8b7b90
mrn1ghtj4r <164870847+ver00123@users.noreply.github.com>
2026-07-05 20:57:21 +0800

    varibles and strings LABORATORY

---
+++ LABORATORY-varibles-strings/LAB-variables-strings.js
@@ -0,0 +1,19 @@
+console.log("Hello! I'm your coding fun fact guide!");
+
+const botName = "ver";
+const botLocation = "PH";
+const favoriteLanguage = "Baybayin language";
+
+console.log("My name is " + botName + " and I live on " + botLocation + ".");
+console.log("My favorite programming language is " + favoriteLanguage + ".");
+
+let codingFact = "i like " + favoriteLanguage + " so much, because it's very simple language.";
+console.log(codingFact);
+
+codingFact = "And " + favoriteLanguage + " was a most understandable programming language for me.";
+console.log(codingFact);
+
+codingFact = "Did you know that " + favoriteLanguage + " originated from the PH, and also " + favoriteLanguage + " is ancient programming language.";
+console.log(codingFact);
+
+console.log("It was fun sharing these facts with you. Goodbye! - " + botName + " from " + botLocation + ".");
