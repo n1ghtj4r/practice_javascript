44c60e012b01583d33ea55aca3af9c50a02afd9b
mrn1ghtj4r <164870847+ver00123@users.noreply.github.com>
2026-07-06 00:07:27 +0800

    varibles and strings, dyanmic, and typeof, sentence maker

---
+++ dynamic-typing/example-dynamic-typing.js
@@ -0,0 +1,15 @@
+let example = "Hello";
+console.log(example);
+
+//changing it in to number or int value
+
+example = 42;
+console.log(example);
+
+//another example of creating a variable with a type set to number then changing it to later be of type string
+
+let data = 100;  // Initially a number
+console.log(data);
+
+data = "New data";  // Dynamically changes to a string
+console.log(data);
---
+++ sentence-maker-varibles-strings/sentences.js
@@ -0,0 +1,21 @@
+let adjective = "Handsome";
+let noun = "Boy";
+let verb = "Reload";
+let place = "Incantadia";
+let adjective2 = "Gun" ;
+let noun2 = "Dragonfruit";
+
+const firstStory = "Once upon a time, there was a(n) " + adjective + " " + noun + " who loved to eat " + noun2 + ". The " + noun + " lived in a " + place + " and had " + adjective2 + " nostrils that blew fire when it was " + verb + ".";
+console.log("First story: " + firstStory);
+
+console.log("");
+
+adjective = "Beautiful";
+noun = "Girl";
+verb = "Reloading water";
+place = "Mars";
+adjective2 = "Water Gun" ;
+noun2 = "Water Melon";
+
+const secondStory = "Once upon a time, there was a(n) " + adjective + " " + noun + " who loved to eat " + noun2 + ". The " + noun + " lived in a " + place + " and had " + adjective2 + " nostrils that blew fire when it was " + verb + ".";
+console.log("Second story: " + secondStory);
