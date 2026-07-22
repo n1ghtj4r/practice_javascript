6792e8a849fc9779ad844857e00a45f71497f346
mrn1ghtj4r <164870847+ver00123@users.noreply.github.com>
2026-07-22 21:10:53 +0800

    boolean, equality and inequality opeators, comparison opeartors lessons

---
+++ boolean-equality-inequality-operators/equality-inequality-operators.js
@@ -0,0 +1,14 @@
+let isOldEnoughToDrive = true;
+
+if (isOldEnoughToDrive) {
+ console.log("You're old enough to drive"); // You're old enough to drive
+} else {
+ console.log("Sorry, you are not old enough to drive");
+}
+
+console.log("\n");
+
+console.log(5 == "5"); // true
+console.log(5 === '5'); // false
+console.log(5 != "5"); // false
+console.log(5 !== "5"); // true
---
+++ comparison-operators/comparison-operators.js
@@ -0,0 +1,33 @@
+let a = 6;
+let b = 9;
+
+console.log(a > b); // false
+console.log(b > a); // true
+
+console.log("\n");
+
+a = 6;
+b = 9;
+let c = 6;
+
+console.log(a >= b); // false
+console.log(b >= a); // true
+console.log(a >= c); // true
+
+console.log("\n");
+
+a = 6;
+b = 9;
+
+console.log(a < b); // true
+console.log(b < a); // false
+
+console.log("\n");
+
+a = 6;
+b = 9;
+c = 6;
+
+console.log(a <= b); // true
+console.log(b <= a); // false
+console.log(a <= c); // true
