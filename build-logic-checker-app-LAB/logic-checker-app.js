751074ff6427ac11a1720606578c9baac75cc2f8
mrn1ghtj4r <164870847+ver00123@users.noreply.github.com>
2026-07-23 21:11:17 +0800

    logic checker app LAB

---
+++ build-logic-checker-app-LAB/logic-checker-app.js
@@ -0,0 +1,25 @@
+const truthyOrFalsy = "";
+//true can have value to be true, while false can have nothing to be false
+console.log(Boolean(truthyOrFalsy));
+
+console.log("\n");
+
+const hasDeveloperJob = true;
+
+if (hasDeveloperJob) {
+  console.log("Timmy is employed as a developer.");
+}
+
+const isTimmyAGamer = false;
+
+if (isTimmyAGamer) {
+  console.log("Timmy loves to play World of Warcraft.");
+}
+
+const timmyAge = 15;
+
+if (timmyAge >= 16) {
+  console.log("Timmy is old enough to drive.");
+} else {
+  console.log("Timmy is not old enough to drive.");
+}
