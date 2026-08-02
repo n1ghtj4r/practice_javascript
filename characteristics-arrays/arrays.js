004d43531a7842a65121bb2d6922ef922b8901f5
mrn1ghtj4r <164870847+ver00123@users.noreply.github.com>
2026-08-02 20:57:10 +0800

    access update elements, and characteristics arrays lessons

---
+++ characteristics-arrays/arrays.js
@@ -0,0 +1,8 @@
+let fruits = ["apple", "banana", "orange"];
+console.log(fruits[0]); // "apple"
+console.log(fruits[2]); // "orange"
+
+console.log("\n");
+
+fruits = ["apple", "banana", "orange"];
+console.log(fruits.length); // 3
